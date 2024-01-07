fd3f09353cdb53d607c113f73f4953a96abf889d
At0micTurtle <carltrep@videotron.ca>
2024-01-07 10:18:54 -0500

    Fix footer background color class

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -64,7 +64,7 @@ export default function Footer() {
 			<div
 				className={`${
 					darkTheme ? 'bg-tertiary-light' : 'bg-tertiary-dark'
-				} 'h-10 md:h-[70px] mt-16 w-full bottom-0 left-0'`}
+				} h-10 md:h-[70px] mt-16 w-full bottom-0 left-0`}
 			/>
 		</footer>
 	);
